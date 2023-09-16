4be209b67a6ca160097c4a1591ff12cb111e33b9
thor8126 <lakhabhayi@gmail.com>
2023-09-16 12:00:15 +0530

    navbar scroll

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elem = document.querySelector('.style-5');
-    var flkty = new Flickity(elem, {
-      cellAlign: 'center',
-      contain: true,
-      prevNextButtons: true,
-      pageDots: false,
-      wrapAround: true,
-      autoPlay: true,
-      pauseAutoPlayOnHover: true
-    });
-  });
+
+
+  // Get a reference to your navbar element
+const navbar = document.getElementById('nav');
+
+// Function to handle the scroll event
+function handleScroll() {
+  // Get the current scroll position
+  const scrollPosition = window.scrollY || window.pageYOffset;
+
+  // Check if the scroll position is greater than or equal to 4 rems (adjust as needed)
+  if (scrollPosition >= 4 * parseFloat(getComputedStyle(document.documentElement).fontSize)) {
+    // Add the CSS class for the scrolled state
+    navbar.classList.add('navbar-scrolled');
+  } else {
+    // Remove the CSS class if not scrolled far enough
+    navbar.classList.remove('navbar-scrolled');
+  }
+}
+
+// Attach the handleScroll function to the scroll event
+window.addEventListener('scroll', handleScroll);
+
   
